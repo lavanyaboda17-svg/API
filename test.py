e6cfe007fd093ea8f59d6befc38106cc253ad554
Lavanya Boda <lavanyaboda17@gmail.com>
2026-06-01 15:42:39 +0530

    new file test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 import re
 
+
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://lavanya:123@localhost:5432/post'
